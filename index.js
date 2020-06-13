044dba417f0678ae6d3fbd25995dc76100e45c5c
Samara Grant <samara.amanda01@gmail.com>
2020-06-13 01:51:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(Samara) {
+  return `Happy holidays, ${Samara}!`
+}
+
+function happyCustomHolidayTo(Easter, Samara) {
+  return `Happy ${Easter}, ${Samara}`
+}
