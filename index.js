654b2532cfdb592324494ce51377101dbc8b1c39
Samara Grant <samara.amanda01@gmail.com>
2020-06-13 02:16:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyCustomHolidayTo(New Year, everyone) {
 }
 
 function holidayCountdown(holiday, days) {
-  return `'It's ${20} days until ${Mother\'s Day}!'`
+  return `It's ${20} days until ${Mother\'s Day}!`
 }
